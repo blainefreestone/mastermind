613b77e3fc143d490bdd8bd612a4cc92b2a8de59
blainefreestone <blainefreestone01@gmail.com>
2023-10-06 00:05:05 -0700

    Still working on comparing the number counts of the guess and the secret code.

--- mastermind.rb
+++ mastermind.rb
@@ -1,7 +1,9 @@
+require 'pry-byebug'
+
 class Board
   def initialize(secret_code)
     @board_status = Array.new(12, Turn.new)
-    @secret_code_values = secret_code.split('')
+    @secret_code_values = secret_code.to_s.split('')
   end
 
   def board_to_text
@@ -10,6 +12,11 @@ class Board
     row_seperator = '    -+---+---+---+---++---+---+-'
     turn_text = ->(turn, turn_number) { (turn_number + 1).to_s.rjust(3) + ' ' + turn.turn_to_text }
     top_border + "\n" + @board_status.each_with_index.map(&turn_text).join(row_seperator + "\n") + bottom_border
+  end
+
+  def count_test
+    @board_status[1].input_guess([1, 2, 3, 4])
+    @board_status[1].get_clues([4, 3, 2, 1])
   end
 end
 
@@ -34,20 +41,33 @@ class Turn
     @guess[0] = guess_values[0]
     @guess[1] = guess_values[1]
     @guess[2] = guess_values[2]
-    @guess[3] = guess_values[0]
+    @guess[3] = guess_values[3]
   end
 
   def get_clues(secret_code_values)
-    exact_position = 0
-    same_color = 0
-
-    [exact_position, same_color]
+    get_same_color_count(secret_code_values)
   end
 
-  def compare_number_counts(secret_code_values)
-    secret_code_number_count = secret_code_values.each_with_object(Hash.new(0)) { |value, number_count| number_count[value] += 1 }
-    guess_number_count = @guess..each_with_object(Hash.new(0)) { |value, number_count| number_count[value] += 1 }
-    [1..6].each { |number| secret_code_number_count[number] < guess_number_count[number] ? secret_code_number_count[number] : guess_number_count[number] }
+  def get_same_color_count(secret_code_values)
+    secret_code_number_count = code_number_count(secret_code_values)
+
+    secret_code_number_count.reduce(0) do |count, key, value|
+      greater_value = guess_number_count[key] > value ? value : guess_number_count[key]
+      count += greater_value
+      count
+    end
+  end
+
+  def code_number_count(secret_code_values)
+    secret_code_values.each_with_object(Hash.new(0)) do |value, number_count|
+      number_count[value] += 1
+    end
+  end
+
+  def guess_number_count
+    @guess.each_with_object(Hash.new(0)) do |value, number_count|
+      number_count[value] += 1
+    end
   end
 end
 
@@ -63,4 +83,5 @@ class Game
   end
 end
 
-puts Board.new(1234).board_to_text
+board = Board.new(1234)
+board.count_test
